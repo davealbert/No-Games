bcf4d9cbcf38232c9a34d7a32ec2e2ca968abdef
Dave Albert <dave.albert@publicisdhealthcare.ie>
2014-02-08 12:52:42 +0000

    update to not change scripts, this was breaking many sites.  Might miss some links now

--- myscript.js
+++ myscript.js
@@ -1,20 +1,114 @@
-var filter_words = [ 'Olympic Games', 'Winter Games', 'winter games', 'olympic games', 'Olympic', 'olympic', 'olympics', 'Olympics', 'sochi', 'Sochi' ];
-for (var i = 0; i < filter_words.length; i++) {
-   //var re = new RegExp(filter_words[i], 'g');
-   //var replace_str = $('body').html().replace(re,'<span class="hideTheThingThatShouldBeHidden">'+filter_words[i]+'</span>');
-   //$('body').html(replace_str);
+$(function() {
+setTimeout(function(){
+      var filter_words = [
+         'Olympic Games',
+         'Winter Games',
+         'winter games',
+         'olympic games',
+         'Olympic',
+         'olympic',
+         'olympics',
+         'Olympics',
+         'sochi',
+         'Sochi',
+         '#Sochi2014',
+         '#sochi2014',
+         '@Sochi2014',
+         '@sochi2014'
+      ];
 
-   //$('*:contains('+filter_words[i]+')').each(function(){
-      //if($(this).children().length < 1) {
-            ////$(this).html('XXXX');
-            //$(this).css("color","black").css("background-color", "black");
-      //}
-   //});
 
-$('*:contains('+filter_words[i]+')', document.body).each(function(){
-  $(this).html($(this).html().replace(
-        new RegExp(filter_words[i], 'g'), '<span class=hideTheThingThatShouldBeHidden>'+filter_words[i]+'</span>'
-  ));
+      var elements = [
+         'span',
+         'p',
+         'h1',
+         'h2',
+         'h3',
+         'h4',
+         'h5',
+         'i',
+         'b',
+         'div'
+         ];
+      for (var i = 0; i < filter_words.length; i++) {
+         var word = filter_words[i];
+         console.log(word);
+         redact(word);
+      };
+
+   });
+
+function redact2(word, el) {
+         var re = new RegExp(word, 'g');
+         $(el + ':contains('+word+')', document.body).each(function() {
+            var newHTML = $(this).html().replace(
+               re,
+               '<span class="hideTheThingThatShouldBeHidden">'+word+'</span>'
+            );
+
+            if (newHTML) {
+               $(this).html(newHTML);
+            }
+         });
+}
+
+function getTextNodesIn(node, includeWhitespaceNodes) {
+    var textNodes = [], whitespace = /^\s*$/;
+
+    function getTextNodes(node) {
+        if (node.nodeType == 3) {
+            if (includeWhitespaceNodes || !whitespace.test(node.nodeValue)) {
+                textNodes.push(node);
+            }
+        } else {
+            for (var i = 0, len = node.childNodes.length; i < len; ++i) {
+                getTextNodes(node.childNodes[i]);
+            }
+        }
+    }
+
+    getTextNodes(node);
+    return textNodes;
+}
+
+
+function redact_TextNodes(word) {
+  var nodes = getTextNodesIn(document.getElementsByTagName('body')[0]);
+  for (idx in nodes) {
+    var node = nodes[idx];
+    //node.nodeValue = node.nodeValue.replace(/[^\s]/g, '█');
+    var re = new RegExp(word, 'g');
+    node.nodeValue = node.nodeValue.replace(re, '███████');
+  }
+}
+
+
+function redact_inputs(word) {
+  var elements = document.getElementsByTagName('input');
+  for (idx in elements) {
+    var e = elements[idx];
+    if (typeof(e.value) != 'string') {
+      continue;
+    }
+    e.value = e.value.replace(/[^\s]/g, '█');
+  }
+}
+
+
+function redact(word) {
+  redact_TextNodes(word);
+  //redact_inputs();
+}
+/*
+   var re = new RegExp(filter_words[i], 'g');
+   var replace_str = $('body').html().replace(re,'<span class="hideTheThingThatShouldBeHidden">'+filter_words[i]+'</span>');
+   $('body').html(replace_str);
+
+   $('*:contains('+filter_words[i]+')').each(function(){
+   if($(this).children().length < 1) {
+//$(this).html('XXXX');
+$(this).css("color","black").css("background-color", "black");
+}
 });
-};
-$('.hideTheThingThatShouldBeHidden').css("color","black").css("background-color", "black");
+*/
+}, 1500);
